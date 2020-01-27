063aae06f05fa7134db8ceba1c364f8470b98a05
Erick J. Gutierrez <50554753+erickJ27@users.noreply.github.com>
2020-01-27 15:00:12 -0300

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Entidades;
+
+namespace DAL
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Usuarios> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlServer(@"Server=DESKTOP-SBR4M50\SQLEXPRESS; Database =SistemaOdontologicoDb;Trusted_Connection =true"));
+        }
+
+    }
+}
---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entidades
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Nombres { get; set; }
+        public string Email { get; set; }
+        public int NivelUsuario { get; set; }
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public DateTime FechaIngreso { get; set; }
+
+    }
+}
