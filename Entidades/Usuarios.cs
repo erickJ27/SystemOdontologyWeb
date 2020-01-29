f6fe4d490fe1eb0df8485d39fdd46e068b545384
Erick J. Gutierrez <50554753+erickJ27@users.noreply.github.com>
2020-01-29 17:20:13 -0300

    Validaciones

--- Entidades/Usuarios.cs
+++ Entidades/Usuarios.cs
@@ -9,9 +9,17 @@ namespace Entidades
         public int UsuarioId { get; set; }
         [Required(ErrorMessage = "El nombre es requerido")]
         public string Nombres { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [Range(minimum: 1, maximum: 2, ErrorMessage = "Nivel de usuario invalido")]
         public int NivelUsuario { get; set; }
+        [Required(ErrorMessage = "El usuario es requerido")]
         public string Usuario { get; set; }
+        [Required(ErrorMessage = "La clave es requerida")]
+        [DataType(DataType.Password)]
         public string Clave { get; set; }
         public DateTime FechaIngreso { get; set; }
 
